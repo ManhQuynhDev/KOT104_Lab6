82f1a1fd0976eed8330f633443e6dc8723b481ed
quynhlm.dev <quynhlm.dev@gmail.com>
2024-05-31 21:54:12 +0700

    ph32353

---
+++ app/src/main/java/com/quynhlm/dev/myapplication/Model/Movie.kt
@@ -0,0 +1,10 @@
+package com.quynhlm.dev.myapplication.Model
+
+data class Movie (
+    val title : String ,
+    var time : String,
+    val imageUrl : String ,
+    val describe : String ,
+    val startUp : String
+){
+}
